beee7e49e646e7f3d8ee6409f784c3677e926e58
Nabeel Parkar <nabeelparkar99@gmail.com>
2021-10-08 08:14:25 +0530

    WIP

---
+++ website/src/lib/components/nav.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+
+    const links: { name: string; link: string }[] = [
+        {
+            name: 'Home',
+            link: '/',
+        },
+        {
+            name: 'What is this?',
+            link: '/what-is-this',
+        },
+    ];
+
+    function isActiveLink(link: string) {
+        console.log($page.path, link);
+        return $page.path === link;
+    }
+</script>
+
+<nav class="max-w-6xl m-auto">
+    <div class="flex justify-between py-6 px-5 ">
+        <span class="text-3xl">Nabeel Parkar</span>
+
+        <ul class="flex space-x-5">
+            {#each links as link}
+                <li class={isActiveLink(link.link) ? 'text-blue-600' : ''}>
+                    <a href={link.link}>{link.name}</a>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</nav>
---
+++ website/src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Nav from '$lib/components/nav.svelte';
+
+    import '../app.css';
+</script>
+
+<Nav />
+
+<slot />
